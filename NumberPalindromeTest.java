edbf4976c845ac41e8d7af85cd35c626ded4d2c2
lod531 <buinovsa@tcd.ie>
2015-02-03 15:53:59 +0000

    Create NumberPalindromeTest.java

---
+++ NumberPalindromeTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class NumberPalindromeTest {
+	
+	@Test
+	public void testReverse() {
+		assertEquals("Testing reverse with 1234567", 7654321, NumberPalindrome.reverse(1234567));
+		assertEquals("Testing reverse with 1111111", 1111111, NumberPalindrome.reverse(1111111));
+	}
+	
+	@Test
+	public void testIsPalindrome() {
+		assertEquals("Testing isPalindrome with 123456", false, NumberPalindrome.isPalindrome(123456));
+		assertEquals("Testing isPalindrome with 123321", true, NumberPalindrome.isPalindrome(123321));
+	}
+	
+	@Test 
+	public void testLargestPalindromeThreeDigitNumberProduct() {
+		assertEquals("Testing LargestPalindromeThreeDigitNumberProduct", 906609, NumberPalindrome.largestPalindromeThreeDigitNumberProduct());
+
+	}
+	
+
+}
